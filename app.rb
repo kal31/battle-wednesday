5f501b6d3971ac0853d1deb10ba5fe9e873549f6
Steve Jones <steve.jones763@gmail.com>
2021-10-04 15:48:46 +0100

    Cute cat

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require 'sinatra'
-get '/hello' do
-    "Hello World"
-  end
+require "sinatra/reloader" if development?
+
+get '/' do
+  "Hello!"
+end
+
+get '/secret' do
+ "I have no idea what I am doing."
+end
+
+get '/super-secret' do
+  "I have no idea what I am doing."
+end
+
+get '/cat' do
+  "<img height='500' style='border-style: dashed; color: red;' src='https://i.imgur.com/jFaSxym.png'/>"
+end
