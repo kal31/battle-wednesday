f4ec7b4eed85f5323f87fb628da610ab0057b95c
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-10-05 14:48:47 +0100

    started POSTed params

--- app.rb
+++ app.rb
@@ -5,13 +5,13 @@ get '/' do
   "Hello!"
 end
 
-get '/secret' do
- "I have no idea what I am doing."
-end
+# get '/secret' do
+#  "I have no idea what I am doing."
+# end
 
-get '/super-secret' do
-  "secret message."
-end
+# get '/super-secret' do
+#   "secret message."
+# end
 # get '/cat' do
 #     "<div style='border: 3px dashed red' >
 #     <img src='http://placekitten.com/500/500'>
@@ -24,10 +24,16 @@ get '/random-cat' do
 end
 
 get '/named-cat' do
-    p params
-    @name = params[:name]
-      erb(:index)
-  end
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+post '/name-form' do
+  p params
+  @name = params[:name]
+  erb(:name_form)
+end
 
 
 # get '/named-cat' do
